fccc8f31b332a5ce94e7f1e37a11267f2ee147e6
Lavanya1972006 <lavanyamugilan3108@gmail.com>
2026-06-12 23:49:40 +0530

    Add files via upload

---
+++ Design-Patterns-And-Principles/ProxyPatternExample/ProxyPatternExample.java
@@ -0,0 +1,61 @@
+// Subject Interface
+interface Image {
+    void display();
+}
+
+// Real Subject
+class RealImage implements Image {
+    private String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromServer();
+    }
+
+    private void loadFromServer() {
+        System.out.println("Loading " + fileName + " from remote server...");
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+}
+
+// Proxy Class
+class ProxyImage implements Image {
+    private String fileName;
+    private RealImage realImage;
+
+    public ProxyImage(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+
+        // Lazy Initialization + Caching
+        if (realImage == null) {
+            realImage = new RealImage(fileName);
+        }
+
+        realImage.display();
+    }
+}
+
+// Test Class
+public class ProxyPatternExample {
+
+    public static void main(String[] args) {
+
+        Image image = new ProxyImage("sample.jpg");
+
+        System.out.println("First Display:");
+        image.display();
+
+        System.out.println();
+
+        System.out.println("Second Display:");
+        image.display();
+    }
+}
