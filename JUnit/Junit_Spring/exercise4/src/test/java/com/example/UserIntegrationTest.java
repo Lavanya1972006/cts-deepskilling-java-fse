4d8c35ee5192ee964660b2326a935a1b99bd0b52
Lavanya1972006 <lavanyamugilan3108@gmail.com>
2026-06-28 06:57:02 +0530

    Add files via upload

---
+++ JUnit/Junit_Spring/exercise4/src/test/java/com/example/UserIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class UserIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testGetUserIntegration() throws Exception {
+
+        User user = new User();
+        user.setId(1L);
+        user.setName("Lavanya");
+        userRepository.save(user);
+
+        mockMvc.perform(get("/users/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Lavanya"));
+    }
+}
