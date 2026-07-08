c287546cd41fe5645e51bcdc5b38d27bd2677019
Lavanya1972006 <lavanyamugilan3108@gmail.com>
2026-07-08 22:11:47 +0530

    Add Spring REST HandsOn 3 implementation

---
+++ Spring-REST/Spring-Rest-HandsOn-3/02_REST_Service_Get_All_Employees/test/EmployeeDaoTest.java
@@ -0,0 +1,14 @@
+package com.cognizant.springlearn;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+import com.cognizant.springlearn.dao.EmployeeDao;
+public class EmployeeDaoTest
+{
+    @Test
+    public void testEmployeeList() 
+    {
+        EmployeeDao dao = new EmployeeDao();
+        assertNotNull(dao.getAllEmployees());
+        dao.getAllEmployees().forEach(System.out::println);
+    }
+}
