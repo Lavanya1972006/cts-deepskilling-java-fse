47c9cdac9eda855e95b19128fc642790730843e7
Lavanya1972006 <lavanyamugilan3108@gmail.com>
2026-07-14 05:53:24 +0530

    Added Microservices exercises

---
+++ Microservices/01-Load-Balancing/03-API-Gateway-Resilience/src/main/java/com/cognizant/apigateway/config/LoadBalancerConfiguration.java
@@ -0,0 +1,31 @@
+package com.cognizant.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadBalancerConfiguration {
+
+    public LoadBalancerConfiguration() {
+        System.out.println("Load Balancer Configuration Loaded");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
